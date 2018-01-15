0471cca7fb8daabac70798b3b004ebcf145be7c5
Peter Yasi <pyasi8192@gmail.com>
2018-01-15 16:27:57 -0500

    ready for distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='python-coincap',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/pyasi/python-coincap',
     download_url='https://github.com/pyasi/python-coincap/archive/master.zip',
     author='Peter Yasi',
@@ -13,7 +13,7 @@ setup(
     module=['coincap'],
     description='Python wrapper around the coincap.io API',
     long_description=open('README.md', 'r').read(),
-    install_requires=['requests', 'requests_cache'],
+    install_requires=['requests'],
     keywords=['cryptocurrency', 'API', 'coinmarketcap', 'BTC', 'Bitcoin', 'LTC', 'Litecoin', 'DOGE', 'Dogecoin',
                 'ETH', 'Ethereum '],
 )
